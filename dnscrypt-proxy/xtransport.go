e7781c10e50f502b01ede8b6178694c2a8025e7c
danny30au <thelakesclub@gmail.com>
2025-12-08 10:57:05 +1100

    Update xtransport.go

--- dnscrypt-proxy/xtransport.go
+++ dnscrypt-proxy/xtransport.go
@@ -113,8 +113,6 @@ func ParseIP(ipStr string) net.IP {
     return net.ParseIP(ipStr)
 }
 
-// If ttl < 0, never expire
-// Otherwise, ttl is set to max(ttl, MinResolverIPTTL)
 func uniqueNormalizedIPs(ips []net.IP) []net.IP {
     if len(ips) == 0 {
         return nil
@@ -170,7 +168,6 @@ func (xTransport *XTransport) saveCachedIP(host string, ip net.IP, ttl time.Dura
     xTransport.saveCachedIPs(host, []net.IP{ip}, ttl)
 }
 
-// Mark an entry as being updated
 func (xTransport *XTransport) markUpdatingCachedIP(host string) {
     xTransport.cachedIPs.Lock()
     item, ok := xTransport.cachedIPs.cache[host]
@@ -248,7 +245,6 @@ func (xTransport *XTransport) rebuildTransport() {
                 }
                 return net.JoinHostPort(host, portStr)
             }
-
             cachedIPs, _, _ := xTransport.loadCachedIPs(host)
             targets := make([]string, 0, len(cachedIPs))
             for _, ip := range cachedIPs {
@@ -258,7 +254,6 @@ func (xTransport *XTransport) rebuildTransport() {
                 dlog.Debugf("[%s] IP address was not cached in DialContext", host)
                 targets = append(targets, formatEndpoint(nil))
             }
-
             dial := func(address string) (net.Conn, error) {
                 if xTransport.proxyDialer == nil {
                     dialer := &net.Dialer{Timeout: timeout, KeepAlive: timeout, DualStack: true}
@@ -266,7 +261,6 @@ func (xTransport *XTransport) rebuildTransport() {
                 }
                 return (*xTransport.proxyDialer).Dial(network, address)
             }
-
             var lastErr error
             for idx, target := range targets {
                 conn, err := dial(target)
@@ -284,16 +278,12 @@ func (xTransport *XTransport) rebuildTransport() {
     if xTransport.httpProxyFunction != nil {
         transport.Proxy = xTransport.httpProxyFunction
     }
-
     clientCreds := xTransport.tlsClientCreds
-
     tlsClientConfig := tls.Config{}
     certPool, certPoolErr := x509.SystemCertPool()
-
     if xTransport.keyLogWriter != nil {
         tlsClientConfig.KeyLogWriter = xTransport.keyLogWriter
     }
-
     if clientCreds.rootCA != "" {
         if certPool == nil {
             dlog.Fatalf("Additional CAs not supported on this platform: %v", certPoolErr)
@@ -304,15 +294,13 @@ func (xTransport *XTransport) rebuildTransport() {
         }
         certPool.AppendCertsFromPEM(additionalCaCert)
     }
-
     if certPool != nil {
         letsEncryptX1Cert := []byte(`-----BEGIN CERTIFICATE-----
- MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAwTzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2VhcmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJuZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBYMTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygch77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6UA5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sWT8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyHB5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UCB5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUvKBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWnOlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTnjh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbwqHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CIrU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkqhkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZLubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KKNFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7UrTkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdCjNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVcoyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPAmRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57demyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+ MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAwTzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2VhcmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJuZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBYMTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygch77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6UA5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sWT8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyHB5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UCB5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUvKBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWnOlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTnjh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbwqHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CIrU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkqhkiGw0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZLubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KKNFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7UrTkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdCjNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVcoyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPAmRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57demyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
  -----END CERTIFICATE-----`)
         certPool.AppendCertsFromPEM(letsEncryptX1Cert)
         tlsClientConfig.RootCAs = certPool
     }
-
     if clientCreds.clientCert != "" {
         cert, err := tls.LoadX509KeyPair(clientCreds.clientCert, clientCreds.clientKey)
         if err != nil {
@@ -325,7 +313,6 @@ func (xTransport *XTransport) rebuildTransport() {
         }
         tlsClientConfig.Certificates = []tls.Certificate{cert}
     }
-
     overrideCipherSuite := len(xTransport.tlsCipherSuite) > 0
     if xTransport.tlsDisableSessionTickets || overrideCipherSuite {
         tlsClientConfig.SessionTicketsDisabled = xTransport.tlsDisableSessionTickets
@@ -397,7 +384,6 @@ func (xTransport *XTransport) rebuildTransport() {
                 uAddr, _ := net.ResolveUDPAddr(network, net.JoinHostPort(targetHost, strconv.Itoa(port)))
                 return udpTarget{addr: uAddr, network: network}
             }
-
             cachedIPs, _, _ := xTransport.loadCachedIPs(host)
             targets := make([]udpTarget, 0, len(cachedIPs))
             for _, ip := range cachedIPs {
@@ -407,7 +393,6 @@ func (xTransport *XTransport) rebuildTransport() {
                 dlog.Debugf("[%s] IP address was not cached in H3 context", host)
                 targets = append(targets, buildAddr(nil))
             }
-
             var lastErr error
             for idx, target := range targets {
                 if target.addr == nil {
@@ -518,3 +503,7 @@ func (xTransport *XTransport) resolveUsingServers(
                 return ips, ttl, nil
             }
             if err == nil {
+                err = errors.New("no IP addresses returned")
+            }
+            lastErr = err
+            dlog.Debugf("Resolver attempt %d fa
