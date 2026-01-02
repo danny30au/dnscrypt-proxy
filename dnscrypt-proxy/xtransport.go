908236581a81a8a2f346cd6e3e709fd0dca0602f
danny30au <thelakesclub@gmail.com>
2026-01-02 13:01:55 +1100

    Add files via upload

--- dnscrypt-proxy/xtransport.go
+++ dnscrypt-proxy/xtransport.go
@@ -914,9 +914,9 @@ func (xTransport *XTransport) Fetch(
                             break
                         }
                         v = strings.TrimSpace(v)
-                        if strings.HasPrefix(v, "h3=":") {
-                            v = strings.TrimPrefix(v, "h3=":")
-                            v = strings.TrimSuffix(v, """)
+                        if strings.HasPrefix(v, `h3=":`) {
+                            v = strings.TrimPrefix(v, `h3=":`)
+                            v = strings.TrimSuffix(v, `"`)
                             if xAltPort, err := strconv.ParseUint(v, 10, 16); err == nil && xAltPort <= 65535 {
                                 altPort = uint16(xAltPort)
                                 dlog.Debugf("Using HTTP/3 for [%s]", url.Host)
