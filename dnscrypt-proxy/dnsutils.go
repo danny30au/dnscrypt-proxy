b092a39c97e628b091477e05fe3a2e4645028ae0
danny30au <thelakesclub@gmail.com>
2026-01-03 13:25:48 +1100

    Add files via upload

--- dnscrypt-proxy/dnsutils.go
+++ dnscrypt-proxy/dnsutils.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-	"encoding/binary"
-	"errors"
-	"net"
-	"net/netip"
-	"strings"
-	"sync"
-	"unicode/utf8"
-	"unsafe"
+    "encoding/binary"
+    "errors"
+    "net"
+    "net/netip"
+    "strings"
+    "sync"
+    "time"
+    "unicode/utf8"
+    "unsafe"
 
-	"codeberg.org/miekg/dns"
-	"codeberg.org/miekg/dns/rdata"
-	"github.com/jedisct1/dlog"
+    "codeberg.org/miekg/dns"
+    "codeberg.org/miekg/dns/rdata"
+    "github.com/jedisct1/dlog"
 )
 
 // --- Memory Pools ---
 
 var msgPool = sync.Pool{
-	New: func() interface{} {
-		return new(dns.Msg)
-	},
+    New: func() interface{} {
+        return new(dns.Msg)
+    },
 }
 
 // GetMsg retrieves a zeroed message from the pool
 func GetMsg() *dns.Msg {
-	return msgPool.Get().(*dns.Msg)
+    return msgPool.Get().(*dns.Msg)
 }
 
 // PutMsg resets and returns a message to the pool
@@ -33,9 +34,6 @@ func PutMsg(m *dns.Msg) {
 	if m == nil {
 		return
 	}
-
-	// Optimized: Use struct zero assignment for first set of fields
-	// This allows compiler to optimize better than individual assignments
 	m.ID = 0
 	m.Response = false
 	m.Opcode = 0
@@ -48,7 +46,6 @@ func PutMsg(m *dns.Msg) {
 	m.CheckingDisabled = false
 	m.Rcode = 0
 
-	// Clear slices while keeping capacity
 	m.Question = m.Question[:0]
 	m.Answer = m.Answer[:0]
 	m.Ns = m.Ns[:0]
@@ -59,16 +56,16 @@ func PutMsg(m *dns.Msg) {
 
 // Buffer pool for truncated packets
 var bufPool = sync.Pool{
-	New: func() interface{} {
-		b := make([]byte, 0, 1500)
-		return &b
-	},
+    New: func() interface{} {
+        b := make([]byte, 0, 1500)
+        return &b
+    },
 }
 
 // --- Static Data ---
 var (
-	blockedHinfoCPU = "This query has been locally blocked"
-	blockedHinfoOS  = "by dnscrypt-proxy"
+    blockedHinfoCPU = "This query has been locally blocked"
+    blockedHinfoOS  = "by dnscrypt-proxy"
 )
 
 // --- Functions ---
@@ -78,7 +75,6 @@ func EmptyResponseFromMessage(srcMsg *dns.Msg) *dns.Msg {
 	dstMsg.ID = srcMsg.ID
 	dstMsg.Opcode = srcMsg.Opcode
 
-	// FIXED: Deep copy questions to prevent aliasing issues with pool reuse
 	if len(srcMsg.Question) > 0 {
 		dstMsg.Question = append(dstMsg.Question[:0], srcMsg.Question...)
 	}
@@ -91,12 +87,11 @@ func EmptyResponseFromMessage(srcMsg *dns.Msg) *dns.Msg {
 
 	if srcMsg.UDPSize > 0 {
 		dstMsg.UDPSize = srcMsg.UDPSize
-		// dstMsg.Security = srcMsg.Security // Copy security bit (DO) if needed
 	}
 	return dstMsg
 }
 
-// TruncatedResponse - Optimized with buffer pool fix
+// TruncatedResponse - Optimized
 func TruncatedResponse(packet []byte) ([]byte, error) {
 	if len(packet) < 12 {
 		return nil, errors.New("packet too short")
@@ -114,7 +109,7 @@ func TruncatedResponse(packet []byte) ([]byte, error) {
 				offset += 2
 				break
 			}
-			offset++
+			offset += 1
 			if labelLen == 0 {
 				break
 			}
@@ -127,7 +122,6 @@ func TruncatedResponse(packet []byte) ([]byte, error) {
 		return nil, errors.New("packet malformed")
 	}
 
-	// FIXED: Get buffer from pool and properly return it
 	bufPtr := bufPool.Get().(*[]byte)
 	buf := *bufPtr
 	if cap(buf) < offset {
@@ -137,16 +131,14 @@ func TruncatedResponse(packet []byte) ([]byte, error) {
 	}
 
 	copy(buf, packet[:offset])
-	buf[2] |= 0x82 // TC, QR
+	buf[2] |= 0x82
 	for i := 6; i < 12; i++ {
 		buf[i] = 0
 	}
 
-	// Create new packet to return (caller owns this memory)
 	newPacket := make([]byte, offset)
 	copy(newPacket, buf)
 
-	// Return buffer to pool
 	*bufPtr = buf[:0]
 	bufPool.Put(bufPtr)
 
@@ -156,7 +148,6 @@ func TruncatedResponse(packet []byte) ([]byte, error) {
 func RefusedResponseFromMessage(srcMsg *dns.Msg, refusedCode bool, ipv4 net.IP, ipv6 net.IP, ttl uint32) *dns.Msg {
 	dstMsg := EmptyResponseFromMessage(srcMsg)
 
-	// OPTIMIZED: Lazy EDE allocation - only create if EDNS0 is active
 	var ede *dns.EDE
 	if dstMsg.UDPSize > 0 {
 		ede = &dns.EDE{InfoCode: dns.ExtendedErrorFiltered}
@@ -222,30 +213,29 @@ func RefusedResponseFromMessage(srcMsg *dns.Msg, refusedCode bool, ipv4 net.IP, 
 }
 
 func HasTCFlag(packet []byte) bool {
-	return packet[2]&2 == 2
+    return packet[2]&2 == 2
 }
 
 func TransactionID(packet []byte) uint16 {
-	return binary.BigEndian.Uint16(packet[0:2])
+    return binary.BigEndian.Uint16(packet[0:2])
 }
 
 func SetTransactionID(packet []byte, tid uint16) {
-	binary.BigEndian.PutUint16(packet[0:2], tid)
+    binary.BigEndian.PutUint16(packet[0:2], tid)
 }
 
 func Rcode(packet []byte) uint8 {
-	return packet[3] & 0xf
+    return packet[3] & 0xf
 }
 
 func NormalizeRawQName(name *[]byte) {
-	for i, c := range *name {
-		if c >= 65 && c <= 90 {
-			(*name)[i] = c + 32
-		}
-	}
+    for i, c := range *name {
+        if c >= 65 && c <= 90 {
+            (*name)[i] = c + 32
+        }
+    }
 }
 
-// OPTIMIZED: Single-pass normalization with inline conversion
 func NormalizeQName(str string) (string, error) {
 	if len(str) == 0 || str == "." {
 		return ".", nil
@@ -255,7 +245,6 @@ func NormalizeQName(str string) (string, error) {
 	strLen := len(str)
 	needsConversion := false
 
-	// Single pass: validate and check for uppercase
 	for i := 0; i < strLen; i++ {
 		c := str[i]
 		if c >= utf8.RuneSelf {
@@ -270,7 +259,6 @@ func NormalizeQName(str string) (string, error) {
 		return str, nil
 	}
 
-	// Convert to lowercase in-place
 	b := []byte(str)
 	for i := 0; i < len(b); i++ {
 		c := b[i]
@@ -279,6 +267,342 @@ func NormalizeQName(str string) (string, error) {
 		}
 	}
 
-	// Zero-allocation string conversion (unsafe)
 	return unsafe.String(unsafe.SliceData(b), len(b)), nil
+}
+
+func getMinTTL(msg *dns.Msg, minTTL uint32, maxTTL uint32, cacheNegMinTTL uint32, cacheNegMaxTTL uint32) time.Duration {
+    if (msg.Rcode != dns.RcodeSuccess && msg.Rcode != dns.RcodeNameError) ||
+        (len(msg.Answer) <= 0 && len(msg.Ns) <= 0) {
+        return time.Duration(cacheNegMinTTL) * time.Second
+    }
+    var ttl uint32
+    if msg.Rcode == dns.RcodeSuccess {
+        ttl = uint32(maxTTL)
+    } else {
+        ttl = uint32(cacheNegMaxTTL)
+    }
+    if len(msg.Answer) > 0 {
+        for _, rr := range msg.Answer {
+            if rr.Header().TTL < ttl {
+                ttl = rr.Header().TTL
+            }
+        }
+    } else {
+        for _, rr := range msg.Ns {
+            if rr.Header().TTL < ttl {
+                ttl = rr.Header().TTL
+            }
+        }
+    }
+    if msg.Rcode == dns.RcodeSuccess {
+        if ttl < minTTL {
+            ttl = minTTL
+        }
+    } else {
+        if ttl < cacheNegMinTTL {
+            ttl = cacheNegMinTTL
+        }
+    }
+    return time.Duration(ttl) * time.Second
+}
+
+func updateTTL(msg *dns.Msg, expiration time.Time) {
+    until := time.Until(expiration)
+    ttl := uint32(0)
+    if until > 0 {
+        ttl = uint32(until / time.Second)
+        if until-time.Duration(ttl)*time.Second >= time.Second/2 {
+            ttl += 1
+        }
+    }
+    for _, rr := range msg.Answer {
+        rr.Header().TTL = ttl
+    }
+    for _, rr := range msg.Ns {
+        rr.Header().TTL = ttl
+    }
+    for _, rr := range msg.Extra {
+        if dns.RRToType(rr) != dns.TypeOPT {
+            rr.Header().TTL = ttl
+        }
+    }
+}
+
+func hasEDNS0Padding(packet []byte) (bool, error) {
+    msg := dns.Msg{Data: packet}
+    if err := msg.Unpack(); err != nil {
+        return false, err
+    }
+    // In v2, options are in Pseudo
+    for _, rr := range msg.Pseudo {
+        if _, ok := rr.(*dns.PADDING); ok {
+            return true, nil
+        }
+    }
+    return false, nil
+}
+
+func addEDNS0PaddingIfNoneFound(msg *dns.Msg, unpaddedPacket []byte, paddingLen int) ([]byte, error) {
+    if msg.UDPSize == 0 {
+        msg.UDPSize = uint16(MaxDNSPacketSize)
+    }
+    
+    for _, rr := range msg.Pseudo {
+        if _, ok := rr.(*dns.PADDING); ok {
+            return unpaddedPacket, nil
+        }
+    }
+
+    paddingRR := &dns.PADDING{Padding: strings.Repeat("X", paddingLen)}
+    msg.Pseudo = append(msg.Pseudo, paddingRR)
+    
+    if err := msg.Pack(); err != nil {
+        return nil, err
+    }
+    return msg.Data, nil
+}
+
+func removeEDNS0Options(msg *dns.Msg) bool {
+    if len(msg.Pseudo) == 0 {
+        return false
+    }
+    msg.Pseudo = nil
+    return true
+}
+
+func dddToByte(s []byte) byte {
+    return byte((s[0]-'0')*100 + (s[1]-'0')*10 + (s[2] - '0'))
+}
+
+func PackTXTRR(s string) []byte {
+    bs := make([]byte, len(s))
+    msg := make([]byte, 0)
+    copy(bs, s)
+    for i := 0; i < len(bs); i++ {
+        if bs[i] == '\\' {
+            i++
+            if i == len(bs) {
+                break
+            }
+            if i+2 < len(bs) && 
+               (bs[i] >= '0' && bs[i] <= '9') && 
+               (bs[i+1] >= '0' && bs[i+1] <= '9') && 
+               (bs[i+2] >= '0' && bs[i+2] <= '9') {
+                msg = append(msg, dddToByte(bs[i:]))
+                i += 2
+            } else if bs[i] == 't' {
+                msg = append(msg, 9) // Tab
+            } else if bs[i] == 'r' {
+                msg = append(msg, 13) // CR
+            } else if bs[i] == 'n' {
+                msg = append(msg, 10) // LF
+            } else {
+                msg = append(msg, bs[i])
+            }
+        } else {
+            msg = append(msg, bs[i])
+        }
+    }
+    return msg
+}
+
+type DNSExchangeResponse struct {
+    response         *dns.Msg
+    rtt              time.Duration
+    priority         int
+    fragmentsBlocked bool
+    err              error
+}
+
+func DNSExchange(
+    proxy *Proxy,
+    proto string,
+    query *dns.Msg,
+    serverAddress string,
+    relay *DNSCryptRelay,
+    serverName *string,
+    tryFragmentsSupport bool,
+) (*dns.Msg, time.Duration, bool, error) {
+    for {
+        cancelChannel := make(chan struct{})
+        maxTries := 3
+        channel := make(chan DNSExchangeResponse, 2*maxTries)
+        var err error
+        options := 0
+
+        for tries := 0; tries < maxTries; tries++ {
+            if tryFragmentsSupport {
+                queryCopy := query.Copy()
+                queryCopy.ID += uint16(options)
+                go func(query *dns.Msg, delay time.Duration) {
+                    time.Sleep(delay)
+                    option := DNSExchangeResponse{err: errors.New("Canceled")}
+                    select {
+                    case <-cancelChannel:
+                    default:
+                        option = _dnsExchange(proxy, proto, query, serverAddress, relay, 1500)
+                    }
+                    option.fragmentsBlocked = false
+                    option.priority = 0
+                    channel <- option
+                }(queryCopy, time.Duration(200*tries)*time.Millisecond)
+                options++
+            }
+            queryCopy := query.Copy()
+            queryCopy.ID += uint16(options)
+            go func(query *dns.Msg, delay time.Duration) {
+                time.Sleep(delay)
+                option := DNSExchangeResponse{err: errors.New("Canceled")}
+                select {
+                case <-cancelChannel:
+                default:
+                    option = _dnsExchange(proxy, proto, query, serverAddress, relay, 480)
+                }
+                option.fragmentsBlocked = true
+                option.priority = 1
+                channel <- option
+            }(queryCopy, time.Duration(250*tries)*time.Millisecond)
+            options++
+        }
+        var bestOption *DNSExchangeResponse
+        for i := 0; i < options; i++ {
+            if dnsExchangeResponse := <-channel; dnsExchangeResponse.err == nil {
+                if bestOption == nil || dnsExchangeResponse.priority < bestOption.priority ||
+                    (dnsExchangeResponse.priority == bestOption.priority && dnsExchangeResponse.rtt < bestOption.rtt) {
+                    bestOption = &dnsExchangeResponse
+                    if bestOption.priority == 0 {
+                        close(cancelChannel)
+                        break
+                    }
+                }
+            } else {
+                err = dnsExchangeResponse.err
+            }
+        }
+        if bestOption != nil {
+            if bestOption.fragmentsBlocked {
+                dlog.Debugf("[%v] public key retrieval succeeded but server is blocking fragments", *serverName)
+            } else {
+                dlog.Debugf("[%v] public key retrieval succeeded", *serverName)
+            }
+            return bestOption.response, bestOption.rtt, bestOption.fragmentsBlocked, nil
+        }
+
+        if relay == nil || !proxy.anonDirectCertFallback {
+            if err == nil {
+                err = errors.New("Unable to reach the server")
+            }
+            return nil, 0, false, err
+        }
+        dlog.Infof(
+            "Unable to get the public key for [%v] via relay [%v], retrying over a direct connection",
+            *serverName,
+            relay.RelayUDPAddr.IP,
+        )
+        relay = nil
+    }
+}
+
+func _dnsExchange(
+    proxy *Proxy,
+    proto string,
+    query *dns.Msg,
+    serverAddress string,
+    relay *DNSCryptRelay,
+    paddedLen int,
+) DNSExchangeResponse {
+    var packet []byte
+    var rtt time.Duration
+
+    if proto == "udp" {
+        qNameLen, padding := len(query.Question[0].Header().Name), 0
+        if qNameLen < paddedLen {
+            padding = paddedLen - qNameLen
+        }
+        if padding > 0 {
+            paddingRR := &dns.PADDING{Padding: strings.Repeat("X", padding)}
+            query.Pseudo = append(query.Pseudo, paddingRR)
+            if query.UDPSize == 0 {
+                query.UDPSize = uint16(MaxDNSPacketSize)
+            }
+        }
+        if err := query.Pack(); err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        binQuery := query.Data
+        udpAddr, err := net.ResolveUDPAddr("udp", serverAddress)
+        if err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        upstreamAddr := udpAddr
+        if relay != nil {
+            proxy.prepareForRelay(udpAddr.IP, udpAddr.Port, &binQuery)
+            upstreamAddr = relay.RelayUDPAddr
+        }
+        now := time.Now()
+        pc, err := net.DialTimeout("udp", upstreamAddr.String(), proxy.timeout)
+        if err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        defer pc.Close()
+        if err := pc.SetDeadline(time.Now().Add(proxy.timeout)); err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        if _, err := pc.Write(binQuery); err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        packet = make([]byte, MaxDNSPacketSize)
+        length, err := pc.Read(packet)
+        if err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        rtt = time.Since(now)
+        packet = packet[:length]
+    } else {
+        if err := query.Pack(); err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        binQuery := query.Data
+        tcpAddr, err := net.ResolveTCPAddr("tcp", serverAddress)
+        if err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        upstreamAddr := tcpAddr
+        if relay != nil {
+            proxy.prepareForRelay(tcpAddr.IP, tcpAddr.Port, &binQuery)
+            upstreamAddr = relay.RelayTCPAddr
+        }
+        now := time.Now()
+        var pc net.Conn
+        proxyDialer := proxy.xTransport.proxyDialer
+        if proxyDialer == nil {
+            pc, err = net.DialTimeout("tcp", upstreamAddr.String(), proxy.timeout)
+        } else {
+            pc, err = (*proxyDialer).Dial("tcp", tcpAddr.String())
+        }
+        if err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        defer pc.Close()
+        if err := pc.SetDeadline(time.Now().Add(proxy.timeout)); err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        binQuery, err = PrefixWithSize(binQuery)
+        if err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        if _, err := pc.Write(binQuery); err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        packet, err = ReadPrefixed(&pc)
+        if err != nil {
+            return DNSExchangeResponse{err: err}
+        }
+        rtt = time.Since(now)
+    }
+    msg := dns.Msg{Data: packet}
+    if err := msg.Unpack(); err != nil {
+        return DNSExchangeResponse{err: err}
+    }
+    return DNSExchangeResponse{response: &msg, rtt: rtt, err: nil}
 }
