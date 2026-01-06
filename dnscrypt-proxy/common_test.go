dfc8170be857cfeddbd99cf0caf6c53ce3dba42d
danny30au <thelakesclub@gmail.com>
2026-01-06 13:48:19 +1100

    Update common_test.go

--- dnscrypt-proxy/common_test.go
+++ dnscrypt-proxy/common_test.go
@@ -5,6 +5,20 @@ import (
 "testing"
 )
 
+var (
+udpUDPAddr = &net.UDPAddr{
+IP:   net.ParseIP("192.168.1.1"),
+Port: 53,
+}
+udpNetAddr net.Addr = udpUDPAddr
+
+tcpTCPAddr = &net.TCPAddr{
+IP:   net.ParseIP("10.0.0.1"),
+Port: 53,
+}
+tcpNetAddr net.Addr = tcpTCPAddr
+)
+
 func TestExtractClientIPStr(t *testing.T) {
 tests := []struct {
 name         string
@@ -25,15 +39,7 @@ wantOK: false,
 name: "valid UDP address",
 pluginsState: &PluginsState{
 clientProto: "udp",
-clientAddr: func() *net.Addr {
-addr := net.Addr(
-&net.UDPAddr{
-IP:   net.ParseIP("192.168.1.1"),
-Port: 53,
-},
-)
-return &addr
-}(),
+clientAddr:  &udpNetAddr,
 },
 wantIP: "192.168.1.1",
 wantOK: true,
@@ -42,15 +48,7 @@ wantOK: true,
 name: "valid TCP address",
 pluginsState: &PluginsState{
 clientProto: "tcp",
-clientAddr: func() *net.Addr {
-addr := net.Addr(
-&net.TCPAddr{
-IP:   net.ParseIP("10.0.0.1"),
-Port: 53,
-},
-)
-return &addr
-}(),
+clientAddr:  &tcpNetAddr,
 },
 wantIP: "10.0.0.1",
 wantOK: true,
@@ -59,15 +57,7 @@ wantOK: true,
 name: "unknown protocol",
 pluginsState: &PluginsState{
 clientProto: "unknown",
-clientAddr: func() *net.Addr {
-addr := net.Addr(
-&net.TCPAddr{
-IP:   net.ParseIP("10.0.0.1"),
-Port: 53,
-},
-)
-return &addr
-}(),
+clientAddr:  &tcpNetAddr,
 },
 wantIP: "",
 wantOK: false,
@@ -75,7 +65,9 @@ wantOK: false,
 }
 
 for _, tt := range tests {
+tt := tt
 t.Run(tt.name, func(t *testing.T) {
+t.Parallel()
 gotIP, gotOK := ExtractClientIPStr(tt.pluginsState)
 if gotIP != tt.wantIP {
 t.Errorf("ExtractClientIPStr() IP = %v, want %v", gotIP, tt.wantIP)
