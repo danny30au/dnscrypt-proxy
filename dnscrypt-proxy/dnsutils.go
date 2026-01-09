dd850c12ab2edbcba484f7724c66f5372f002414
danny30au <thelakesclub@gmail.com>
2026-01-09 11:58:17 +1100

    Update dnsutils.go

--- dnscrypt-proxy/dnsutils.go
+++ dnscrypt-proxy/dnsutils.go
@@ -367,7 +367,7 @@ func PackTXTRR(s string) []byte {
 
     for i := 0; i < len(s); i++ {
         c := s[i]
-        if c !=
+        if c !== /""/
             buf.WriteByte(c) {
             continue
         }
