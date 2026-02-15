42e5cf9c515ca8284686d7124ca717d84e7a0689
danny30au <thelakesclub@gmail.com>
2026-02-16 00:10:08 +1100

    Add files via upload

--- dnscrypt-proxy/plugin_ecs.go
+++ dnscrypt-proxy/plugin_ecs.go
@@ -9,6 +9,12 @@ import (
 	"github.com/jedisct1/dlog"
 )
 
+const (
+	inetFamilyIPv4 uint16 = 1
+	inetFamilyIPv6 uint16 = 2
+)
+
+// PluginECS sets EDNS Client Subnet (ECS) information in outgoing queries.
 type PluginECS struct {
 	nets []*net.IPNet
 }
@@ -23,7 +29,11 @@ func (plugin *PluginECS) Description() string {
 
 func (plugin *PluginECS) Init(proxy *Proxy) error {
 	plugin.nets = proxy.ednsClientSubnets
-	dlog.Notice("ECS plugin enabled")
+	if len(plugin.nets) == 0 {
+		dlog.Warn("ECS plugin enabled, but no EDNS client subnets configured")
+		return nil
+	}
+		dlog.Notice("ECS plugin enabled")
 	return nil
 }
 
@@ -36,49 +46,58 @@ func (plugin *PluginECS) Reload() error {
 }
 
 func (plugin *PluginECS) Eval(pluginsState *PluginsState, msg *dns.Msg) error {
-	// Check if SUBNET already exists in Pseudo section
+	if len(plugin.nets) == 0 {
+		return nil
+	}
+
+	// If ECS already exists, do nothing.
 	for _, rr := range msg.Pseudo {
 		if _, ok := rr.(*dns.SUBNET); ok {
 			return nil
 		}
 	}
 
-	// Enable EDNS0 if not already enabled
+	// Ensure EDNS0 is enabled.
 	if msg.UDPSize == 0 {
 		msg.UDPSize = uint16(pluginsState.maxPayloadSize)
 	}
 
-	// Create SUBNET option
 	ipnet := plugin.nets[rand.Intn(len(plugin.nets))]
-	bits, totalSize := ipnet.Mask.Size()
+	maskBits, addrBits := ipnet.Mask.Size()
 
-	var family uint16
-	var addr netip.Addr
-	if totalSize == 32 {
-		family = 1
-		if ip4 := ipnet.IP.To4(); ip4 != nil {
-			addr = netip.AddrFrom4([4]byte(ip4))
-		} else {
-			return nil
-		}
-	} else if totalSize == 128 {
-		family = 2
-		if ip6 := ipnet.IP.To16(); ip6 != nil {
-			addr = netip.AddrFrom16([16]byte(ip6))
-		} else {
-			return nil
-		}
-	} else {
+	family, addr, ok := ipNetToECSAddress(ipnet, addrBits)
+	if !ok {
 		return nil
 	}
 
 	subnet := &dns.SUBNET{
 		Family:  family,
-		Netmask: uint8(bits),
+		Netmask: uint8(maskBits),
 		Scope:   0,
 		Address: addr,
 	}
 	msg.Pseudo = append(msg.Pseudo, subnet)
 
 	return nil
+}
+
+func ipNetToECSAddress(ipnet *net.IPNet, addrBits int) (uint16, netip.Addr, bool) {
+	switch addrBits {
+	case 32:
+		ip4 := ipnet.IP.To4()
+		if ip4 == nil {
+			return 0, netip.Addr{}, false
+		}
+		return inetFamilyIPv4, netip.AddrFrom4([4]byte(ip4)), true
+
+	case 128:
+		ip6 := ipnet.IP.To16()
+		if ip6 == nil {
+			return 0, netip.Addr{}, false
+		}
+		return inetFamilyIPv6, netip.AddrFrom16([16]byte(ip6)), true
+
+	default:
+		return 0, netip.Addr{}, false
+	}
 }
